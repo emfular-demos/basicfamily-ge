2a910ffc67aab844141efedd6f00be9bb83af6d0
Anonymous <anonymous@example.com>
2026-03-18 21:39:11 +0100

    Styling copied, plus first reference like before - tutorial only uses father and mother setting

--- src/app/basicfamily/details/person-details/person-details.component.ts
+++ src/app/basicfamily/details/person-details/person-details.component.ts
@@ -1,13 +1,37 @@
 import {Component, Input} from '@angular/core';
 import {Person} from "../../core/Person";
+import {FormsModule} from "@angular/forms";
+import {ContainerDetailsComponent} from "ngx-emfular-integration";
+import {BasicfamilyService} from "../../edit/Basicfamily.service";
+import {PersonDetailsService} from "../person-details.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-person-details',
-  imports: [],
+  imports: [
+    FormsModule,
+    ContainerDetailsComponent,
+    NgForOf
+  ],
   templateUrl: './person-details.component.html',
   styleUrl: './person-details.component.css'
 })
 export class PersonDetailsComponent {
   @Input() person!: Person
+
+  constructor(
+      public modelService: BasicfamilyService,
+      public detailsService: PersonDetailsService,
+      ) {}
+
+  addChild() {
+
+  }
+  removeChild(child: Person) {
+
+  }
+  openDetails(person: Person) {
+    this.detailsService.openDetails(person);
+  }
 
 }
